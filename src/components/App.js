352eeb4aab57c074b07747d2d23d9f7e9fb6b2a4
Sarat Chandra <sarat1669@gmail.com>
2018-02-20 10:21:10 +0530

    initial commit

---
+++ src/components/App.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import { Switch, Route } from 'react-router-dom'
+
+import Home from './Home'
+import PageNotFound from './PageNotFound'
+
+export default function App() {
+  return (
+    <div>
+      <h1>react-popped</h1>
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route component={PageNotFound} />
+      </Switch>
+    </div>
+  )
+}
---
+++ src/components/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Interactive from 'react-interactive'
+
+import { Link } from 'react-router-dom'
+
+export default function Home() {
+  return (
+    <div>
+      Home
+    </div>
+  )
+}
